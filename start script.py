72ea6fea941d4c395d99d55de5e1738535c7c882
Gihaad3 <gihaadahmed2003@gmail.com>
2023-11-06 14:40:34 +0100

    Ik laat de spanning van de min naar de max oplopen

--- start script.py
+++ start script.py
@@ -5,3 +5,7 @@ ports = rm.list_resources()
 device = rm.open_resource(
     "ASRL9::INSTR", read_termination="\r\n", write_termination="\n"
 )
+
+
+for x in range(0, 1024):
+     device.query(f"OUT:CH0 {x}")
