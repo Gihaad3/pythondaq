3d9e751209ffee8896911681e02f4e8776303681
Gihaad3 <gihaadahmed2003@gmail.com>
2023-11-06 13:57:16 +0100

    verandering labels. lijst voor spanningen LED gemaakt. Spanningen worden toegevoegd.

--- Quick 'n dirty meting.py
+++ Quick 'n dirty meting.py
@@ -5,8 +5,12 @@ ports = rm.list_resources()
 device = rm.open_resource(
     "ASRL9::INSTR", read_termination="\r\n", write_termination="\n"
 )
+
+U_LED = []
 for x in range (0, 1024):
     device.query(f"OUT:CH0 {x}")
     U_tot = device.query("MEAS:CH1?") 
     U_2 = device.query("MEAS:CH2?") 
-    U_LED = U_tot - U_2
+    U_1 = U_tot - U_2
+    U_LED.append(U_1)
+
