22c915367c047b5d8f2b780049b0f4bc1e28c362
Gihaad3 <gihaadahmed2003@gmail.com>
2023-11-06 14:02:07 +0100

    Ik verander de variabelen in integers.

--- Quick 'n dirty meting.py
+++ Quick 'n dirty meting.py
@@ -12,8 +12,7 @@ for x in range (0, 1024):
     device.query(f"OUT:CH0 {x}")
     U_tot = device.query("MEAS:CH1?") 
     U_2 = device.query("MEAS:CH2?") 
-    U_1 = U_tot - U_2
+    U_1 = int(U_tot) - int(U_2)
     U_LED.append(U_1)
-    I = U_1 / 220
+    I = int(U_1) / 220
     I_LED.append(I)
-
