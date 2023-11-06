8d0da7359452814392f742a6470bb340f75b7932
Gihaad3 <gihaadahmed2003@gmail.com>
2023-11-06 15:31:10 +0100

    Ik print de spanning over de LED en de weerstand

--- start script.py
+++ start script.py
@@ -11,8 +11,9 @@ device = rm.open_resource(
 
 for x in range(0, 1024):
      device.query(f"OUT:CH0 {x}")
-     U_2 = int(device.query("MEAS: CH2?"))
+     U_2 = int(device.query("MEAS:CH2?"))
      V_2 = 3.3/1023 * U_2
-     U_tot = int(device.query("MEAS: CH1?"))
+     U_tot = int(device.query("MEAS:CH1?"))
      U_1 = U_tot - U_2
      V_1 = 3.3/1023 * U_1
+     print(f"On LED:  {U_1} ({V_1} V)    Over resistor:  {U_2} ({V_2} V)")
