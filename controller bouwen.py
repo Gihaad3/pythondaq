2cb569444f7ad3eb87cc0d63ac519b49035b9056
Gihaad3 <gihaadahmed2003@gmail.com>
2023-11-09 11:12:13 +0100

    Ik import de controller in het model.

--- controller bouwen.py
+++ controller bouwen.py
@@ -1,23 +1,9 @@
 import pyvisa
 import matplotlib.pyplot as plt
 import csv
-from arduino_device import ArduinoVISADevice, list_devices
-
-port = "ASRL9::INSTR"
-device = ArduinoVISADevice(port=port)
 
 # Bij set_output_value zetten we een spanning over het circuit.
 # Bij get_output_value return je de spaniong over het circuit.
-
-# for x in range (0, 1024):
-#     device.set_output_value(x)
-#     U_tot = device.get_output_value() 
-#     U_2 = device.get_input_value(channel = 2) 
-#     U_1 = int(U_tot) - int(U_2)
-#     device.U_LED.append(U_1)
-#     I = int(U_1) / 220
-#     device.I_LED.append(I)
-# device.set_output_value(0)
 
 with open('metingen.csv', 'w', newline='') as csvfile:
      writer = csv.writer(csvfile)
@@ -25,23 +11,22 @@ with open('metingen.csv', 'w', newline='') as csvfile:
      for a, b in zip(device.U_LED, device.I_LED):
         writer.writerow([a, b])
 
-
-
-
 class DiodeExperiment():
     def __init__(self):
-        device=ArduinoVISADevice
+        from arduino_device import ArduinoVISADevice, list_devices
+        port = "ASRL9::INSTR"
+        self.device = ArduinoVISADevice(port=port)
     def scan(self):
         for value in range(0,1023):
-            device.set_output_value(value)
-            U_tot = device.get_output_value() 
-            U_2 = device.get_input_value(channel = 2) 
+            self.device.set_output_value(value)
+            U_tot = self.device.get_output_value() 
+            U_2 = self.device.get_input_value(channel = 2) 
             U_1 = int(U_tot) - int(U_2)
-            device.U_LED.append(U_1)
+            self.device.U_LED.append(U_1)
             I = int(U_1) / 220
-            device.I_LED.append(I)
-        device.set_output_value(0)
-        return device.U_LED, device.I_LED
+            self.device.I_LED.append(I)
+        self.device.set_output_value(0)
+        return self.device.U_LED, self.device.I_LED
 
 model=DiodeExperiment()
 data = model.scan()
