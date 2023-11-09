22994c613b507f06684a5a292fd376ceaaceca9c
Gihaad3 <gihaadahmed2003@gmail.com>
2023-11-09 11:30:37 +0100

    Nu maak ik een csv bestand dmv het model.

--- controller bouwen.py
+++ controller bouwen.py
@@ -4,12 +4,6 @@ from diode_experiment import DiodeExperiment
 # Bij set_output_value zetten we een spanning over het circuit.
 # Bij get_output_value return je de spaniong over het circuit.
 
-with open('metingen.csv', 'w', newline='') as csvfile:
-     writer = csv.writer(csvfile)
-     writer.writerow(['U', 'I'])
-     for a, b in zip(device.U_LED, device.I_LED):
-        writer.writerow([a, b])
-
 model=DiodeExperiment()
 data = model.scan(100, 1000)
 U = data[0]
@@ -17,3 +11,9 @@ I = data[1]
 
 plt.plot(U, I)
 plt.show()
+
+with open('metingen.csv', 'w', newline='') as csvfile:
+     writer = csv.writer(csvfile)
+     writer.writerow(['U', 'I'])
+     for a, b in zip(data[0], data[1]):
+        writer.writerow([a, b])
