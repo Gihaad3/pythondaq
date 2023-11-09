14580f6022d493086b20d8c3a0300277bd7f4d8c
Gihaad3 <gihaadahmed2003@gmail.com>
2023-11-09 12:42:20 +0100

    Ik heb de lijsten op de juiste plek.

--- diode_experiment.py
+++ diode_experiment.py
@@ -1,17 +1,24 @@
+import numpy as np
+import statistics
+
 class DiodeExperiment():
     def __init__(self):
         from arduino_device import ArduinoVISADevice
         from arduino_device import list_devices
         port = "ASRL9::INSTR"
         self.device = ArduinoVISADevice(port=port)
+        self.U_LED = []
+        self.I_LED = []
+        self.U_af = []
+        self.I_af = []
     def scan(self, min, max):
         for value in range(min, max):
             self.device.set_output_value(value)
             U_tot = self.device.get_output_value() 
             U_2 = self.device.get_input_value(channel = 2) 
             U_1 = int(U_tot) - int(U_2)
-            self.device.U_LED.append(U_1)
+            self.U_LED.append(U_1)
             I = int(U_1) / 220
-            self.device.I_LED.append(I)
+            self.I_LED.append(I)
         self.device.set_output_value(0)
-        return self.device.U_LED, self.device.I_LED
+        return self.U_LED, self.I_LED
