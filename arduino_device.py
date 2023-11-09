24805b93d3bd627536654066210ec17920c21100
Gihaad3 <gihaadahmed2003@gmail.com>
2023-11-09 10:19:54 +0100

    Ik heb de controller afgesplitst

---
+++ arduino_device.py
@@ -0,0 +1,35 @@
+import pyvisa
+
+class ArduinoVISADevice():
+   
+    def __init__(self, port):
+        rm = pyvisa.ResourceManager("@py")
+        self.device = rm.open_resource(
+            "ASRL9::INSTR", read_termination="\r\n", write_termination="\n"\
+        )
+
+        self.U_LED = []
+        self.I_LED = []
+
+    def get_identification(self):
+        return self.device.query("*IDN?")
+        
+    def set_output_value(self, value):
+        self.device.query(f"OUT:CH0 {value}")
+
+    def get_output_value(self):
+        return self.device.query(f"MEAS:CH1?")
+
+    def get_input_value(self, channel):
+        return self.device.query(f"MEAS:CH{channel}?")
+
+
+    def get_input_voltage(self, channel=2):
+        V= 3.3/1023 * int(self.device.query(f"MEAS:CH{channel}?"))
+        return V
+
+
+def list_devices():
+    rm = pyvisa.ResourceManager("@py")
+    return rm.list_resources()
+
--- controller bouwen.py
+++ controller bouwen.py
@@ -1,45 +1,13 @@
 import pyvisa
 import matplotlib.pyplot as plt
 import csv
-
-class ArduinoVISADevice():
-   
-    def __init__(self, port):
-        rm = pyvisa.ResourceManager("@py")
-        self.device = rm.open_resource(
-            "ASRL9::INSTR", read_termination="\r\n", write_termination="\n"\
-        )
-
-        self.U_LED = []
-        self.I_LED = []
-
-    def get_identification(self):
-        return self.device.query("*IDN?")
-        
-    def set_output_value(self, value):
-        self.device.query(f"OUT:CH0 {value}")
-
-    def get_output_value(self):
-        return self.device.query(f"MEAS:CH1?")
-
-    def get_input_value(self, channel):
-        return self.device.query(f"MEAS:CH{channel}?")
-
-
-    def get_input_voltage(self, channel=2):
-        V= 3.3/1023 * int(self.device.query(f"MEAS:CH{channel}?"))
-        return V
+from arduino_device import ArduinoVISADevice, list_devices
 
 port = "ASRL9::INSTR"
 device = ArduinoVISADevice(port=port)
 
 # Bij set_output_value zetten we een spanning over het circuit.
 # Bij get_output_value return je de spaniong over het circuit.
-
-def list_devices():
-    rm = pyvisa.ResourceManager("@py")
-    return rm.list_resources()
-
 
 for x in range (0, 1024):
     device.set_output_value(x)
