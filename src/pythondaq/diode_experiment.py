593fa8de204e24639bea841e0e6eebe222b1013f
Gihaad3 <gihaadahmed2003@gmail.com>
2023-11-16 09:43:21 +0100

    I added the standar deviation calculation and errorbars

--- src/pythondaq/diode_experiment.py
+++ src/pythondaq/diode_experiment.py
@@ -1,12 +1,9 @@
 import numpy as np
 import statistics
-from pythondaq.arduino_device import ArduinoVISADevice
-from pythondaq.arduino_device import list_devices
-from statistics import mean
+from pythondaq.arduino_device import ArduinoVISADevice, list_devices
 
-# Deze class stuurt een spanning door een elektrisch 
-# circuit en meet de spanning over en de stroomsterkte door 
-# een LED
+# Deze class stuurt een spanning door een elektrisch circuit 
+# en meet de spanning over en de stroomsterkte door een LED
 class DiodeExperiment():
 
     # Deze method bewaart alle hergebruikte informatie
@@ -14,12 +11,11 @@ class DiodeExperiment():
         port = "ASRL9::INSTR"
         self.device = ArduinoVISADevice(port=port)
 
-        # Deze lijsten slaan de spanning en stroomsterkte
-        # apart en samen op
+        # Deze lijsten slaan de spanning en stroomsterkte apart en samen op
         self.U_LED = []
         self.I_LED = []
         self.U_I = []
-
+        self.alle_data =[]
     # Deze method voert het experiment uit
     def scan(self, min, max):
         for value in range(min, max):
@@ -33,3 +29,44 @@ class DiodeExperiment():
             self.U_I.append([U_lamp, I])
         self.device.set_output_value(0)
         return self.U_LED, self.I_LED, self.U_I
+
+    # Deze method voert het experiment een aantal keer 
+    # uit en slaat alle data op in één lijst
+    def runexperiment(self, herhalingen, min, max):
+        self.alle_data = []
+
+	    #Ik herhaal het experiment en gebruik daarbij de nested list met U en I
+        for i in range(herhalingen):
+            experiment = DiodeExperiment.scan(self, min, max)[2]
+		   
+            #Ik ga elke waarde af in een individueel experiment en stop het in één lijst
+            for waarde in experiment:
+                self.alle_data.append(waarde)
+        return self.alle_data  
+
+    #Deze method berekent de standaard deviatie
+    def std_calc(self,min,max):
+        standaard_deviaties = []
+        gemidelde_I = []
+	
+	    #Ik ga de spannings interval af
+        for volt in range(min,max):
+
+	        # Ik zet de lijst met stroomsterktes telkens op nul
+            I_lijst = []
+		    # Ik ga elke waarde af in de complete set
+            # van de herhaalde experimenten
+            for waarde in self.alle_data:
+	        
+            # waarde is in dit geval een lijst met spanning en stroomsterkte
+		    # Ik sorteer de lijst hier, ik ga elke spanningswaarde
+            # af en zet de bijberhornde stroomsterkte in een lijst
+                if waarde[0] == volt:
+                    I_lijst.append(waarde[1])
+		    
+            # Ik bereken het gemidelde en de standaard deviatie
+            gem_I = statistics.mean(I_lijst)
+            std_I = np.std(I_lijst)
+            standaard_deviaties.append(std_I)
+            gemidelde_I.append(gem_I)
+        return gemidelde_I, standaard_deviaties
--- src/pythondaq/view.py
+++ src/pythondaq/view.py
@@ -1,20 +1,23 @@
 import matplotlib.pyplot as plt
 import csv
 from pythondaq.diode_experiment import DiodeExperiment
-from statistics import mean
-import numpy as np
-
 
 # Deze functie geeft door dat het experiment 
 # moet worden uitgevoerd
 def view():
    # Ik neem de data uit de model
    model=DiodeExperiment()
-   data = model.scan(100, 1000)
+   data = model.scan(0, 1023)
    U = data[0]
    I = data[1]
-   # Ik plot alles
+   error = model.std_calc(0, 1023)
+
+	#Ik plot alles
    plt.scatter(U, I)
+   plt.errorbar(U, error[0], yerr = error[1])
+   plt.xlabel("Spanning in Volt")
+   plt.ylabel("Stroomsterkte in Ampere")
+
    plt.show()
 
    #Ik sla de data op in een csv bestand
