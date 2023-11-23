5a0e20d2dce00834882d3514f758553ae11be413
Gihaad3 <gihaadahmed2003@gmail.com>
2023-11-23 09:24:31 +0100

    I added the option to determen the min and max volt

--- src/pythondaq/cli.py
+++ src/pythondaq/cli.py
@@ -14,12 +14,23 @@ def cmd_group():
 def list():
     return print("hallo")
 
+
+
+
 @cmd_group.command()
 @click.option(
-
-    default=1,
-    help="print",
-    show_default=True,
+    "--min",
+    default=0,
+    help="this gives the minimum voltage over the circuit.",
+    show_default=True
 )
+
+@click.option(
+    "--max",
+    default=1023,
+    help="this gives the maximum voltage over the circuit.",
+    show_default=True
+)
+
 def scan():
     return print("nee")
