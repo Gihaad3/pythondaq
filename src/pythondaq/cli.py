52243f2b68e827ca01d9994fa0c71927439e64f4
Gihaad3 <gihaadahmed2003@gmail.com>
2023-11-23 09:40:40 +0100

    I added the option to output the measurments as a csv file

--- src/pythondaq/cli.py
+++ src/pythondaq/cli.py
@@ -1,6 +1,7 @@
 import click
 from pythondaq.arduino_device import ArduinoVISADevice, list_devices
 from pythondaq.diode_experiment import DiodeExperiment
+import csv
 
 @click.group()
 def cmd_group():
@@ -17,7 +18,7 @@ def cmd_group():
 #     return print("hallo")
 
 
-
+# THE ABOVE FUNCTION IS TYEMPORARLY COMMENTED, I WILL USE IT SOON 
 
 @cmd_group.command()
 @click.option(
@@ -33,11 +34,23 @@ def cmd_group():
     help="this gives the maximum voltage over the circuit.",
     show_default=True
 )
-
-def scan(min, max):
+@click.option(
+    "--output",
+    help="this outputs the data in a csv ddocument.",
+    show_default = True,
+)
+def scan(min, max, output):
     model = DiodeExperiment()
     data = model.scan(min, max, N=1)
     measurments = []
     for volt in range(min, max):
         measurments.append([data[3][volt], data[1][volt]])
+    
+    if output is not None:       
+        with open(f'{output}.csv', 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(['U', 'I'])
+            for a, b in zip(data[3], data[1]):
+                writer.writerow([a, b])
+       
     return print(measurments)
