385ed9a4936a0685fb5d88bac6c44e0e47dc623d
Gihaad3 <gihaadahmed2003@gmail.com>
2023-11-30 09:26:55 +0100

    I now collect the data from the arduino

--- src/pythondaq/new.py
+++ src/pythondaq/new.py
@@ -1,10 +1,15 @@
 import sys
+from pythondaq.diode_experiment import DiodeExperiment
 
 from PySide6 import QtWidgets
 
 
 class UserInterface(QtWidgets.QMainWindow):
-    pass
+    def __init__():
+        super.__init__()
+        model = DiodeExperiment()
+        data = model.scan(0, 1023, 100)
+        return data
 
 
 def main():
