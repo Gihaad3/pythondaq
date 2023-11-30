f3f00d0167f82a370a65c6f7add3fc23b44c256f
Gihaad3 <gihaadahmed2003@gmail.com>
2023-11-30 09:34:19 +0100

    I added the method to plot the data

--- src/pythondaq/new.py
+++ src/pythondaq/new.py
@@ -1,15 +1,26 @@
 import sys
 from pythondaq.diode_experiment import DiodeExperiment
-
 from PySide6 import QtWidgets
-
+from PySide6.QtCore import Slot
+from PySide6 import QtWidgets
+import numpy as np
 
 class UserInterface(QtWidgets.QMainWindow):
-    def __init__():
+    def __init__(self):
         super.__init__()
         model = DiodeExperiment()
         data = model.scan(0, 1023, 100)
+
+        self.plot_widget = pg.PlotWidget()
         return data
+
+    @Slot()
+    def plot(self):
+        self.plot_widget.clear()
+        x = np.linspace(-np.pi, np.pi, 100)
+        self.plot_widget.plot(x, np.sin(x), symbol=None, pen={"color": "k", "width": 5})
+        self.plot_widget.setLabel("left", "sin(x)")
+        self.plot_widget.setLabel("bottom", "x [radians]")
 
 
 def main():
